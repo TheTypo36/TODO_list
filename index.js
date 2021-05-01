0024b07ac98fe66f18973b2f7f77471240031a8b
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-01 11:48:13 +0530

    add view engine as ejs and add view for home page

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
-
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.use('/', require('./routes/index.js'));
 app.listen(port, function (err) {
     if (err) {
